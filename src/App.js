f51fc2e19238ed1e8d007a4c25bbdc520d79b2c5
saurabh kumar <35069331+saurabhpojhi@users.noreply.github.com>
2022-01-13 12:18:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+import  Temp from "./weather/temp";
+const App = () => {
+  return (
+    <>
+      < Temp />
+    </>
+  );
+};
+
+export default App;
